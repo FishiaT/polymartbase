8fd530b8b18f337e5d23887c047d546a387aa623
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2023-01-26 07:23:51 +0700

    bug fix 1

--- base.py
+++ base.py
@@ -169,8 +169,9 @@ async def user_token_response(ctx, response: str):
     resourcesCount = 0
     ownedResources = 0
     member = ctx.member
-    user_data = await PolymartAPI.getUserData(api_key, user_id)
-    user_name, user_id = await getUser(response, configData['global-api-key'], user_data)
+    user_id = await PolymartAPI.verifyUser(user_token)
+    user_data = await PolymartAPI.getUserData(configData['global-api-key'], user_id)
+    user_name = await getUser(response, configData['global-api-key'], user_data)
     base_success_text_part_1 = "Username: " + user_name + "\nUser ID: " + user_id + "\n\nStatus:"
     base_success_text_part_2 = "\n\nVerification Successfully!"
     final_success_text = ""
@@ -181,7 +182,7 @@ async def user_token_response(ctx, response: str):
         specificKey = configData['global-api-key']
         if resourceList[r].getResourceSpecificKey() is not None:
             specificKey = resourceList[r].getResourceSpecificKey()
-        ownershipStatus = await checkAndVerify(ctx, specificKey, resourceList[r].getResourceID(), response, resourceList[r].getResourceRoleID(), user_data)
+        ownershipStatus = await checkAndVerify(ctx, specificKey, resourceList[r].getResourceID(), response, resourceList[r].getResourceRoleID(), user_data, user_id)
         if ownershipStatus == True:
             ownedResources += 1
             verbose(str(ctx.author.name) + "#" + str(ctx.author.discriminator) + " owns resource " + str(resourceList[r].getResourceName()))
@@ -203,14 +204,11 @@ async def user_token_response(ctx, response: str):
     
 async def getUser(user_token, api_key, user_data):
     verbose("getUser() called, parameters: " + str(user_token) + ", " + str(api_key))
-    user_id = await PolymartAPI.verifyUser(user_token)
-    user_name = user_data['response']['user']['username']
-    verbose("getUser() success, results: " + str(user_name) + ", " + str(user_id))
-    return user_name, user_id
+    verbose("getUser() success, results: " + str(user_name))
+    return user_data['response']['user']['username']
     
-async def checkAndVerify(context, api_key, resource_id, user_token, verified_role_id, user_data):
-    verbose("checkAndVerify() called, parameters: " + str(api_key) + ", " + str(resource_id) + ", " + str(user_token) + ", " + str(verified_role_id))
-    user_id = await PolymartAPI.verifyUser(user_token)
+async def checkAndVerify(context, api_key, resource_id, user_token, verified_role_id, user_data, user_id):
+    verbose("checkAndVerify() called, parameters: " + str(api_key) + ", " + str(resource_id) + ", " + str(user_token) + ", " + str(verified_role_id) + ", " + str(user_id))
     resource_user_data = await PolymartAPI.getResourceUserData(api_key, resource_id, user_id)
     if context.author.roles and verified_role_id not in context.author.roles or not context.author.roles:
         if resource_user_data['response']['resource']['purchaseValid']:
