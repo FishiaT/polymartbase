912ca6d077fa6bb7be5219a6635db1a154af59d0
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2023-05-20 14:25:27 +0700

    Version 4.0.0

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # polymartbase
  Self hosted configurable Discord bot for verifying Polymart plugins ownership.
 
-## Initial setup
- You will need Python 3.8 or newer installed.
+## Installation
+ You will need Python 3.10 or newer installed.
 
- Then, run `pip install aiohttp git+https://github.com/interactions-py/library.git@unstable ruamel.yaml` to install the needed dependencies. 
+ Run `pip install httpx discord-py-interactions ruamel.yaml` to install the needed dependencies. 
 
- Now clone this repository, edit `config.yml` and `resources.yml` (every config options is documented), then run the bot by running `python3 base.py`. If configured properly, the bot should go online. Now anyone can run `/verify` and follow the instructions to verify themselves!
+ Now clone this repository, adjust the options in `config.yml` and `resources.yml`, then run the bot by running `python3 bot.py`. If configured properly, the bot should go online.
+
+## Configuration
+ Most configuration options are properly documented inside the config files. Change the options accordingly.
 
 ## License
  This project is licensed under the permissive MIT license.
  
- tl/dr: do whatever you want with this bot, including selling it, I don't care. But you must credit me. I'm not liable for anything that you have done.
+ tl/dr: do whatever you want, including selling it as long as you credit me properly.
--- base.py
+++ base.py
@@ -1,207 +1,187 @@
 # PolymartBase
-# Licensed under MIT license
-
-# Powered by terrible code written by a dumb weeb
 
 from ruamel import yaml
 from pathlib import Path
-import aiohttp
-import random
+import httpx
 import json
 import interactions
-import os
 import datetime
 
-yaml1 = yaml.YAML(typ='safe')
-configData = yaml1.load(Path('config.yml'))
-resourceData = yaml1.load(Path('resources.yml'))
+bot_version = "4.0.0"
 
-# Bot version
-# i'm a bit fucked with versioning rn
-bot_version = "3.2.3"
+config_data = yaml.YAML(typ='safe').load(Path('config.yml'))
+resource_data = yaml.YAML(typ='safe').load(Path('resources.yml'))
 
-# Git branch and whether it is open source or closed source
-bot_branch = "oss/main"
+resources = {}
 
-class PolymartAPI:
-   async def generateVerifyURL():
-       url = configData['base-url'] + "/v1/generateUserVerifyURL"
-       arg = {'service':configData['service']}
-       token = None
-       async with aiohttp.ClientSession() as session:
-          async with session.get(url, json=arg) as r:
-             token = json.loads(str(await r.text()))['response']['result']['url']
-             return token
-
-   async def verifyUser(token):
-       url = configData['base-url'] + "/v1/verifyUser"
-       arg = {'service':configData['service'],'token':token}
-       async with aiohttp.ClientSession() as session:
-          async with session.get(url, json=arg) as r:
-             id = json.loads(str(await r.text()))['response']['result']['user']['id']
-             return id
-
-   async def getUserData(api_key, user_id):
-       url = configData['base-url'] + "/v1/getUserData"
-       arg = {'api_key':api_key,'user_id':user_id}
-       async with aiohttp.ClientSession() as session:
-          async with session.post(url, json=arg) as r:
-              return json.loads(str(await r.text()))
-
-   async def getResourceUserData(api_key, resource_id, user_id):
-       url = configData['base-url'] + "/v1/getResourceUserData"
-       arg = {'api_key':api_key,'resource_id':resource_id,'user_id':user_id}
-       async with aiohttp.ClientSession() as session:
-          async with session.post(url, json=arg) as r:
-             return json.loads(str(await r.text()))
-
-   async def getAccountType(user_id):
-       url = configData['base-url'] + "/v1/getAccountInfo"
-       arg = {'user_id':user_id}
-       async with aiohttp.ClientSession() as session:
-          async with session.post(url, json=arg) as r:
-             try:
-                # why tf do you need to do this
-                # the type property is enough lol
-                return json.loads(str(await r.text()))['response']['user']['type']
-             except:
-                return json.loads(str(await r.text()))['response']['team']['type']
+bot = interactions.Client(intents=interactions.Intents.DEFAULT)
 
 class Resource:
-    resourceName = None
-    resourceID = None
-    resourceRoleID = None
-    resourceIcon = None
-    resourceSpecificKey = None
-    def __init__(self, resourceName, resourceID, resourceRoleID, resourceIcon, resourceSpecificKey):
-        self.resourceName = resourceName
-        self.resourceID = resourceID
-        self.resourceRoleID = resourceRoleID
-        self.resourceIcon = resourceIcon
-        self.resourceSpecificKey = resourceSpecificKey
-    def getResourceName(self):
-        return self.resourceName
-    def getResourceID(self):
-        return self.resourceID
-    def getResourceRoleID(self):
-        return self.resourceRoleID
-    def getResourceIcon(self):
-        return self.resourceIcon
-    def getResourceSpecificKey(self):
-        return self.resourceSpecificKey
-
-def parsePlaceholder(ctx, string):
-    return string.replace("%version%", bot_version).replace("%branch%", bot_branch).replace("%date%", str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M"))).replace("%user_name%", str(ctx.author.name)).replace("%user_discriminator%", str(ctx.author.discriminator)).replace("%user_full%", str(ctx.author.name) + "#" + str(ctx.author.discriminator))
-
-print("PolymartBase " + str(bot_version))
-print("Licensed under the permissive MIT license")
-bot = interactions.Client(token=configData['bot-token'])
-if configData['activity'] is not None:
-    bot = interactions.Client(token=configData['bot-token'], presence=interactions.ClientPresence(activities=[interactions.PresenceActivity(name=configData['activity'], type=interactions.PresenceActivityType.GAME)]))
-
-# Commands
-
-@bot.command(
-    name="verify",
-    description="Verify ownership of plugins",
-    scope=configData['server-id']
-)
-async def verify(ctx: interactions.CommandContext):
-   channel_id = ctx.channel.id
-   if configData['channel-id'] is not None:
-       if configData['channel-id'] != channel_id:
-           errEmbed = interactions.Embed(description=configData['indicator']['x'] + " Sorry, you cannot use this command in this channel!")
-           await ctx.send(embeds=errEmbed, ephemeral=True)
-           return
-   getTokenButton = interactions.Button(
-        style=interactions.ButtonStyle.LINK,
-        label="Get Token",
-        url=await PolymartAPI.generateVerifyURL()
-   )
-   verifyButton = interactions.Button(
-        style=interactions.ButtonStyle.SUCCESS,
-        label="Verify",
-        custom_id="verify_c"
-   )
-   row = interactions.ActionRow(
-    components=[getTokenButton, verifyButton]
-   )
-   infoEmbed = interactions.Embed(title="Hi there!", description="Hi there " + str(ctx.author.name) + "#" + str(ctx.author.discriminator) + "! This form is here to help you get started! \n\nTo get started, click the **Get Token** button and it will automatically open a link. \nLogin into your Polymart account and it will generate a token. \nCopy that token and click the **Verify** button, enter your token and let the bot handle the rest! \n\nIf you have bought a plugin after verification and would like to get verified for that plugin, just do verify again!", color=0x33a343)
-   infoEmbed.set_footer(text=parsePlaceholder(ctx, configData['layout']['footer']))
-   await ctx.send(embeds=infoEmbed, components=row, ephemeral=True)
-
-@bot.component("verify_c")
-async def verify_component(ctx):
-    tokenInput = interactions.TextInput(
-        style=interactions.TextStyleType.SHORT,
-        label="Enter your Polymart token",
-        custom_id="user_token",
-    )
-    modal = interactions.Modal(
-        title="Your Polymart Token",
-        custom_id="user_token_response",
-        components=[tokenInput],
-    )
-    await ctx.popup(modal)
-
-@bot.modal("user_token_response")
-async def user_token_response(ctx, response: str):
-    await ctx.defer()
-    resourceList = await getAllResources()
-    member = ctx.member
-    user_id = await PolymartAPI.verifyUser(response)
-    user_data = await PolymartAPI.getUserData(configData['global-api-key'], user_id)
-    user_name = await getUser(response, configData['global-api-key'], user_data)
-    base_first = ""
-    base_final = ""
-    final_text = ""
-    embed = interactions.Embed(title=parsePlaceholder(ctx, configData['layout']['title']), color=0x33a343)
-    embed.set_footer(text=parsePlaceholder(ctx, configData['layout']['footer']))
-    if configData['layout']['display-user-avatar'] == True:
-        embed.set_thumbnail(url=member.user.avatar_url)
-    if configData['layout']['mode'] == "new":
-        embed.add_field(name="**User Name**", value=user_name, inline=True)
-        embed.add_field(name="**User ID**", value=str(user_id), inline=True)
-        embed.add_field(name="**Date**", value=str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M")), inline=True)
-    elif configData['layout']['mode'] == "default":
-        base_first = "Username: " + user_name + "\nUser ID: " + user_id + "\n\nStatus:"
-        base_final = "\n\nVerified Successfully!"
-    for r in resourceList:
-        specificKey = configData.get('global-api-key', None) or resourceList[r].getResourceSpecificKey()
-        ownershipStatus = await checkAndVerify(ctx, specificKey, resourceList[r].getResourceID(), response, resourceList[r].getResourceRoleID(), user_data, user_id)
-        if configData['layout']['mode'] == "new" and ownershipStatus:
-            final_text += resourceList[r].getResourceIcon() + " " + resourceList[r].getResourceName() + "\n"
-        elif configData['layout']['mode'] == "default":
-            final_text += "\n" + resourceList[r].getResourceIcon() + " **" + resourceList[r].getResourceName() + "**: " + str(ownershipStatus)
-    if configData['layout']['mode'] == "new":
-        embed.add_field(name="**Owned**", value=final_text)
-        embed.add_field(name="Verified Successfully!", value="")
-        embed.description = "• Discord: " + str(ctx.author.name) + "#" + str(ctx.author.discriminator) + "\n• Account Type: " + str(await PolymartAPI.getAccountType(user_id)).replace("user", "User").replace("team", "Team")
-    elif configData['layout']['mode'] == "default":
-        text = base_first + final_text + base_final
-        embed.description = text.replace("False", configData['indicator']['x']).replace("True", configData['indicator']['check_mark']).replace("None", "")
-    await ctx.send(embeds=embed)
+    name = None
+    id = None
+    role_id = None
+    icon = None
+    api_key = None
+    def __init__(self, name, id, role_id, icon, api_key):
+        self.name = name
+        self.id = id
+        self.role_id = role_id
+        self.icon = icon
+        self.specific_key = api_key
+        
+class PolymartAPI:
+   def generate_verify_url():
+       url = config_data['base_url'] + "/v1/generateUserVerifyURL"
+       arg = {'service':config_data['service']}
+       resp = json.loads(str(httpx.get(url, params=arg).text))
+       return resp
+   def verify_user(token):
+       url = config_data['base_url'] + "/v1/verifyUser"
+       arg = {'service':config_data['service'],'token':token}
+       resp = json.loads(str(httpx.get(url, params=arg).text))
+       return resp
+   def get_user_data(api_key, user_id):
+       url = config_data['base_url'] + "/v1/getUserData"
+       arg = {'api_key':api_key,'user_id':user_id}
+       resp = json.loads(str(httpx.post(url, json=arg).text))
+       return resp
+   def get_resource_user_data(api_key, resource_id, user_id):
+       url = config_data['base_url'] + "/v1/getResourceUserData"
+       arg = {'api_key':api_key,'resource_id':resource_id,'user_id':user_id}
+       resp = json.loads(str(httpx.post(url, json=arg).text))
+       return resp
+                
+@interactions.listen()
+async def on_ready():
+    if config_data['activity'] is not None:
+        await bot.change_presence(status=interactions.Status.ONLINE, activity=interactions.Activity(name=config_data['activity']))
+    if config_data['disable_failsafe']:
+        print("WARNING: BUILT-IN FAILSAFE HAS BEEN DISABLED! IT IS RECOMMENDED TO ENABLE THIS.")
+    print("PolymartBase " + bot_version)
+    print("The bot is now ready!")
     
-async def getUser(user_token, api_key, user_data):
-    return user_data['response']['user']['username']
+async def api_failsafe(ctx, json_data, err_type, resource=None):
+    if json_data['response']['success'] == True or config_data['disable_failsafe']:
+        return True
+    error_embed = interactions.Embed()
+    error_embed.color = "#" + config_data['layout']['error_color']
+    error_embed.footer = "Bot version " + bot_version
+    match err_type:
+        case "verify_url":
+            error_embed.description = config_data['indicator']['x'] + " Failed to generate the verification URL!"
+            await ctx.send(embeds=error_embed, ephemeral=True)
+            return False
+        case "invalid_token":
+            error_embed.description = config_data['indicator']['x'] + " Invalid verification key!"
+            await ctx.send(embeds=error_embed, ephemeral=True)
+            return False
+        case "user_data":
+            error_embed.description = config_data['indicator']['x'] + " Failed to get user information!"
+            await ctx.send(embeds=error_embed, ephemeral=True)
+            return False
+        case "res_user_data":
+            error_embed.description = config_data['indicator']['x'] + " Failed to get user resource ownership information for " + str(resource.name) + "!"
+            await ctx.send(embeds=error_embed, ephemeral=True)
+            return False
     
-async def checkAndVerify(context, api_key, resource_id, user_token, verified_role_id, user_data, user_id):
-    resource_user_data = await PolymartAPI.getResourceUserData(api_key, resource_id, user_id)
-    if context.author.roles and verified_role_id not in context.author.roles or not context.author.roles:
+#@interactions.listen(disable_default_listeners=True)
+#async def on_command_error(event: interactions.api.events.Error):
+#    await event.ctx.send("Something has went wrong, please try again.", ephemeral=True)
+               
+@interactions.slash_command(name="verify", description="Verify plugins ownership.")
+async def verify(ctx: interactions.SlashContext):
+    #if ctx.author.id != 713566850650341437:
+    #    await ctx.send("Hey there! The bot is currently in a rewrite. As such, the bot is not available to use at the moment. Please use tickets to verify yourself for the time being. Sorry for the inconvenience.", ephemeral=True)
+    #    return
+    if config_data['channel_id'] is not None and ctx.channel.id not in config_data['channel_id']:
+        error_embed = interactions.Embed(description=config_data['indicator']['x'] + " Sorry, you can\'t use this command here!")
+        await ctx.send(embeds=error_embed, ephemeral=True)
+        return
+    url = PolymartAPI.generate_verify_url()
+    if not await api_failsafe(ctx, url, "verify_url"):
+        return
+    inst_buttons: list[interactions.ActionRow] = [
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.URL,
+                label="Get Key",
+                url=url['response']['result']['url'],
+            ),
+            interactions.Button(
+                custom_id="verify_btn",
+                style=interactions.ButtonStyle.GREEN,
+                label="Verify",
+            )
+        )
+    ]
+    instruction_embed = interactions.Embed(title="Just a few more steps...", description="To verify yourself, follow the following steps:\n\n1. Click \"Get Key\", login into your Polymart account if asked, and copy the generated key.\n\n2. Click \"Verify\", then paste the copied key into the text box and click \"Submit\".\n\nThe bot will finish the job automatically after that.", footer="Bot version " + bot_version)
+    await ctx.send(embeds=instruction_embed, components=inst_buttons, ephemeral=True)
+    
+@interactions.listen()
+async def on_component(event: interactions.api.events.Component):
+    ctx = event.ctx
+    match ctx.custom_id:
+        case "verify_btn":
+            token_input = interactions.Modal(
+                interactions.ShortText(
+                    label="Polymart Verification Key",
+                    custom_id="token",
+                    required=True,
+                    placeholder="XXX-XXX-XXX",
+                    min_length=11,
+                    max_length=11,
+                ),
+                title="Enter your verification key.",
+            )
+            await ctx.send_modal(modal=token_input)
+            modal_ctx: interactions.ModalContext = await ctx.bot.wait_for_modal(token_input)
+            verify_resp = PolymartAPI.verify_user(modal_ctx.responses['token'])
+            if not await api_failsafe(modal_ctx, verify_resp, "invalid_token"):
+                return
+            user_id = verify_resp['response']['result']['user']['id']
+            user_data_resp = PolymartAPI.get_user_data(config_data['global_api_key'], user_id)
+            if not await api_failsafe(modal_ctx, user_data_resp, "user_data"):
+                return
+            user_name = user_data_resp['response']['user']['username']
+            await modal_ctx.defer()
+            summary_embed = interactions.Embed(title="Success", color="#" + config_data['layout']['summary_color'], footer="Bot version " + bot_version, description="**• Discord**: " + str(ctx.author.display_name) + "#" + str(ctx.author.discriminator) + "\n**• Date**: " + str(datetime.datetime.now().strftime("%m/%d/%Y")))
+            summary_embed.add_field(name="**User Name**", value=user_name, inline=True)
+            summary_embed.add_field(name="**User ID**", value=str(user_id), inline=True)
+            owned_resources_text = ""
+            owned_res = 0
+            total_res = len(resources)
+            for res in resources:
+                api_key = None
+                if resources[res].api_key is not None:
+                    api_key = resources[res].api_key
+                else:
+                    api_key = config_data['global_api_key']
+                resource_user_data = PolymartAPI.get_resource_user_data(api_key, resources[res].id, user_id)
+                if not await api_failsafe(ctx, resource_user_data, "res_user_data", resources[res]):
+                    return
+                if await verify_ownership(modal_ctx, resources[res], resource_user_data, user_id):
+                    purchase_time = datetime.datetime.fromtimestamp(int(resource_user_data['response']['resource']['purchaseTime'])).strftime("%m/%d/%Y")
+                    owned_resources_text += resources[res].icon + " " + resources[res].name + " (" + str(purchase_time) + ")\n" 
+                    owned_res += 1
+            owned_resources_text += "\n**Verified Successfully!**"
+            summary_embed.add_field(name="**Owned (" + str(owned_res) + "/" + str(total_res) + ")**", value=owned_resources_text)
+            await modal_ctx.send(embeds=summary_embed)
+
+async def verify_ownership(ctx, resource, resource_user_data, user_id):
+    if ctx.author.roles and resource.role_id in ctx.author.roles:
+        return True
+    elif ctx.author.roles and resource.role_id not in ctx.author.roles or not ctx.author.roles:
         if resource_user_data['response']['resource']['purchaseValid']:
-            await context.author.add_role(verified_role_id, configData['server-id'])
+            await ctx.author.add_role(resource.role_id)
             return True
         else:
             return False
-    elif context.author.roles and verified_role_id in context.author.roles:
-        return True
 
-async def getAllResources():
-    resourceList = {}
-    for r in resourceData:
-        resource = resourceData[r]
-        resourceConf = Resource(resource['resource-name'], resource['resource-id'], resource['role-id'], resource['icon'], resource['api-key'])
-        resourceList[r] = resourceConf
-    return resourceList
-            
-bot.start()
+def fetch_resources():
+    for res in resource_data:
+        resource = resource_data[res]
+        resource_conf = Resource(resource['resource_name'], resource['resource_id'], resource['role_id'], resource['icon'], resource['api_key'])
+        resources[res] = resource_conf
+
+fetch_resources()
+bot.start(config_data['bot_token'])
